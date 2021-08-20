d4ca7fe21211d9c2bc807124fb8db338a0c6ac39
Lee-Jun-Young <leejy8506@naver.com>
2021-08-21 00:10:02 +0900

    CategoryOptionActivity 구현 완료

--- app/src/main/java/com/smparkworld/daangnmarket/ui/main/addLife/CategoryOptionActivity.kt
+++ app/src/main/java/com/smparkworld/daangnmarket/ui/main/addLife/CategoryOptionActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.smparkworld.daangnmarket.DaangnApp
 import com.smparkworld.daangnmarket.R
 import com.smparkworld.daangnmarket.databinding.ActivityCategoryOptionBinding
 
@@ -13,8 +17,11 @@ class CategoryOptionActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        categoryOptionBinding = DataBindingUtil.setContentView(this, R.layout.activity_category_option)
+        categoryOptionBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_category_option)
         categoryOptionBinding.categoryOption = this@CategoryOptionActivity
+
+        setRecyclerview()
     }
 
     override fun onPause() {
@@ -22,27 +29,49 @@ class CategoryOptionActivity : AppCompatActivity(), View.OnClickListener {
         overridePendingTransition(0, 0)
     }
 
+    private fun setRecyclerview() {
+        val totalValue: MutableMap<String, *>? = DaangnApp.prefs.getAll()
+        val data = ArrayList<String>()
+        totalValue?.forEach { (name, value) ->
+            if (value as Boolean) {
+                data.add(name)
+            }
+        }
+        if (data.isNotEmpty())
+            categoryOptionBinding.tvSecondSubTitle.isVisible = true
+        else
+            categoryOptionBinding.tvSecondSubTitle.isGone = true
+
+        val adapter = CategoryOptionAdapter(this, data)
+        categoryOptionBinding.categoryOptionRecyclerview.layoutManager = LinearLayoutManager(this)
+        categoryOptionBinding.categoryOptionRecyclerview.adapter = adapter
+        categoryOptionBinding.categoryOptionRecyclerview.setHasFixedSize(true)
+        adapter.notifyDataSetChanged()
+    }
+
     override fun onClick(v: View?) {
-        when(v?.id){
+        when (v?.id) {
             R.id.iv_back -> {
                 finish()
             }
             R.id.tv_categoryOptionOne -> {
-                sendResult(applicationContext.getString(R.string.activityCategoryOption_firstSubTitle_firstText))
+                sendResult(this.getString(R.string.activityCategoryOption_firstSubTitle_firstText))
                 finish()
             }
             R.id.tv_categoryOptionTwo -> {
-                sendResult(applicationContext.getString(R.string.activityCategoryOption_firstSubTitle_secondText))
+                sendResult(this.getString(R.string.activityCategoryOption_firstSubTitle_secondText))
                 finish()
             }
+
         }
     }
 
     private fun sendResult(string: String) {
         val intent = Intent(this, AddLifeActivity::class.java).apply {
-            putExtra("result",string)
+            putExtra("result", string)
             addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
         }
         setResult(RESULT_OK, intent)
     }
+
 }
---
+++ app/src/main/java/com/smparkworld/daangnmarket/ui/main/addLife/CategoryOptionAdapter.kt
@@ -0,0 +1,49 @@
+package com.smparkworld.daangnmarket.ui.main.addLife
+
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.smparkworld.daangnmarket.databinding.CategoryoptionItemBinding
+
+class CategoryOptionAdapter(
+    private var context: Context,
+    val data: ArrayList<String>,
+) :
+    RecyclerView.Adapter<CategoryOptionAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = CategoryoptionItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CategoryOptionAdapter.ViewHolder, position: Int) {
+        holder.bind(data[position])
+
+        holder.itemView.setOnClickListener {
+            val s = data[position]
+            val intent = Intent(context, AddLifeActivity::class.java).apply {
+                putExtra("result",s)
+                addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            }
+            (context as Activity).setResult(RESULT_OK, intent)
+            (context as Activity).finish()
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return data.count()
+    }
+
+    class ViewHolder(private val binding: CategoryoptionItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: String) {
+            binding.tvCategoryOptions.text = data
+        }
+    }
+
+}
